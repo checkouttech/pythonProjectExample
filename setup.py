29cdebaa390495094e14517f03c9778b81beffbd
Kapil Mahant <kapilmahant@Kapils-Macbook.local>
2020-04-05 13:13:39 -0700

    template of build file

---
+++ setup.py
@@ -0,0 +1,69 @@
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+from setuptools import setup, Command
+import os
+import subprocess
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        pytest.main(self.pytest_args)
+
+
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info')
+
+setup(
+    name='pythonProjectExample',
+    version='0.15',
+    # release is not supported in bdist rpm
+    release=subprocess.check_output(["git", "rev-list", "--count", "--first-parent", "HEAD"]).rstrip(),
+    license='BSD',
+    author='gyeh',
+    author_email='hello@world.com',
+    url='http://www.hello.com',
+    long_description="README.txt",
+    #install_requires=['bottle','requests','supervisor'],  # currently not working
+    #dependency_links = ['https://pypi.python.org/packages/source/b/bottle/bottle-0.12.8.tar.gz'],
+    packages=['pythonProjectExample', 'pythonProjectExample.images','pythonProjectExample.docs'],
+    include_package_data=True,
+    package_data={'pythonProjectExample.images' : ['hello.gif']},
+    data_files=[
+                ('/etc/init.d/', ['pythonProjectExamplectl']),
+                ('/var/log/pythonProjectExample',[]),
+                ('/etc/pythonProjectExample/conf/',['conf/supervisord.conf','conf/pythonProjectExample.conf'])
+                ],
+    description="Hello World testing setuptools",
+    tests_require=['pytest'],
+    cmdclass = {
+                'test': PyTest,
+                'clean': CleanCommand
+                }
+
+)
+
+
+
+
+
+
+
